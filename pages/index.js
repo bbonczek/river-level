d8cecbbc8d207862e22d085c9366bef8edb1e13f
Bartek <bartosz.baczek@emphie.com>
2022-01-17 21:20:15 +0100

    stations/[url] added and got rid of unused things

--- pages/index.js
+++ pages/index.js
@@ -14,20 +14,22 @@ export async function getStaticProps() {
 }
 
 export default function Home({ stations }) {
-  console.log(stations);
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Select a river for which you want to see its current water level.</p>
+        <p>
+          Select a hydrological station for which you want to see its current
+          water level.
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Stations</h2>
         <div className="grid">
-          {stations.map(({ id, stationName, river }) => (
-            <Link href={`/stations/${id}`} key={id} passHref>
+          {stations.map(({ stationName, river, url }) => (
+            <Link href={`/stations/${url}`} key={url} passHref>
               <div className="card">
                 <h3>{stationName}</h3>
                 <p>{river}</p>
@@ -35,19 +37,6 @@ export default function Home({ stations }) {
             </Link>
           ))}
         </div>
-        {/* <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul> */}
       </section>
       <style jsx>{`
         .grid {
@@ -56,7 +45,12 @@ export default function Home({ stations }) {
           justify-content: space-between;
           gap: 1rem;
         }
+
         .card {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-between;
+
           flex-basis: 45%;
           padding: 1.5rem;
           text-align: left;
@@ -65,25 +59,27 @@ export default function Home({ stations }) {
           border: 1px solid #eaeaea;
           border-radius: 10px;
           transition: color 0.15s ease, border-color 0.15s ease;
+          cursor: pointer;
         }
+
         .card:hover,
         .card:focus,
         .card:active {
           color: #0070f3;
           border-color: #0070f3;
         }
+
         .card h3 {
           margin: 0 0 1rem 0;
           font-size: 1.5rem;
         }
+
         .card p {
           margin: 0;
           font-size: 1.25rem;
           line-height: 1.5;
         }
-        .logo {
-          height: 1em;
-        }
+
         @media (max-width: 600px) {
           .grid {
             width: 100%;
