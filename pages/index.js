f4bafffecdd1ca6e9111b98b6a163b4303b5c9cc
Bartek <bartosz.baczek@emphie.com>
2022-01-17 19:36:50 +0100

    index.js updated, static data provided

--- pages/index.js
+++ pages/index.js
@@ -2,34 +2,40 @@ import Head from "next/head";
 import Link from "next/link";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from "../lib/posts";
-import Date from "../components/date";
+import { getStations } from "../lib/stations";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const stations = getStations();
   return {
     props: {
-      allPostsData,
+      stations,
     },
   };
 }
 
-export default function Home({ allPostsData }) {
+export default function Home({ stations }) {
+  console.log(stations);
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>It&apos;s a me, Bartek!</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Select a river for which you want to see its current water level.</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
+        <h2 className={utilStyles.headingLg}>Stations</h2>
+        <div className="grid">
+          {stations.map(({ id, stationName, river }) => (
+            <Link href={`/stations/${id}`} key={id} passHref>
+              <div className="card">
+                <h3>{stationName}</h3>
+                <p>{river}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+        {/* <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
               <Link href={`/posts/${id}`}>
@@ -41,8 +47,50 @@ export default function Home({ allPostsData }) {
               </small>
             </li>
           ))}
-        </ul>
+        </ul> */}
       </section>
+      <style jsx>{`
+        .grid {
+          display: flex;
+          flex-direction: row;
+          justify-content: space-between;
+          gap: 1rem;
+        }
+        .card {
+          flex-basis: 45%;
+          padding: 1.5rem;
+          text-align: left;
+          color: inherit;
+          text-decoration: none;
+          border: 1px solid #eaeaea;
+          border-radius: 10px;
+          transition: color 0.15s ease, border-color 0.15s ease;
+        }
+        .card:hover,
+        .card:focus,
+        .card:active {
+          color: #0070f3;
+          border-color: #0070f3;
+        }
+        .card h3 {
+          margin: 0 0 1rem 0;
+          font-size: 1.5rem;
+        }
+        .card p {
+          margin: 0;
+          font-size: 1.25rem;
+          line-height: 1.5;
+        }
+        .logo {
+          height: 1em;
+        }
+        @media (max-width: 600px) {
+          .grid {
+            width: 100%;
+            flex-direction: column;
+          }
+        }
+      `}</style>
     </Layout>
   );
 }
